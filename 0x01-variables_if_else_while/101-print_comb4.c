cc1a94f13c35a377afd404617af75167cde27cc2
ELMACHHOUNE <mohamed20rida@gmail.com>
2023-03-17 02:11:39 -0700

    change code 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
 
-/**
- *main - print all combinations of three different digits
- *Return: Always 0 (Success)
- */
+int main(void) {
+  int i, j;
 
-int main(void)
-{
-	int one;
-	int ten;
-	int hundred;
+  for (i = 0; i < 100; i++) {
+    for (j = i; j < 100; j++) {
+      putchar((i / 10) + '0');
+      putchar((i % 10) + '0');
+      putchar(' ');
+      putchar((j / 10) + '0');
+      putchar((j % 10) + '0');
 
-	for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
-	{
-		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
-		{
-			for (one = (ten + 1); one <= '9'; one++) /*ones*/
-			{
-				putchar(hundred);
-				putchar(ten);
-				putchar(one);
-				if (hundred != '7' || ten != '8' || one != '9')
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
+      if (j != 99) {
+        putchar(',');
+        putchar(' ');
+      }
+    }
+  }
 
-	return (0);
+  return 0;
 }
+

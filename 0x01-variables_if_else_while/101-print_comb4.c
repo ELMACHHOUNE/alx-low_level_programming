b14ddcfd03497772776b09c0e1e58b9e85e0056d
ELMACHHOUNE <mohamed20rida@gmail.com>
2023-03-17 02:15:08 -0700

    change code 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
-int main(void) {
-  int i, j;
+int main(void)
+{
+    int i, j;
 
-  for (i = 0; i < 100; i++) {
-    for (j = i; j < 100; j++) {
-      putchar((i / 10) + '0');
-      putchar((i % 10) + '0');
-      putchar(' ');
-      putchar((j / 10) + '0');
-      putchar((j % 10) + '0');
+    for (i = 0; i <= 98; i++)
+    {
+        for (j = i + 1; j <= 99; j++)
+        {
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(' ');
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
 
-      if (j != 99) {
-        putchar(',');
-        putchar(' ');
-      }
+            if (i != 98 || j != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
     }
-  }
 
-  return 0;
+    putchar('\n');
+
+    return 0;
 }
 

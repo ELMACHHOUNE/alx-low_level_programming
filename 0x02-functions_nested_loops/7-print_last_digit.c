b0fd4bce804062a975463078b9bf0ac4b4d0be86
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-03-21 10:57:08 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,20 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
-       	_putchar('0' + (n % 10));
-	return (n % 10);
+	int last_digit;
+
+    if (n == INT_MIN) {
+        n /= 10;
+        n = -n;
+    }
+
+    if (n < 0) {
+        n = -n;
+    }
+
+    last_digit = n % 10;
+
+    _putchar('0' + last_digit);
+
+    return last_digit;
 }

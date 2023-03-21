193658363746ea0c3c55230cf9694af877a5dfd1
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-03-21 10:28:35 +0000

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 50 Fibonacci numbers
- *
- * Return: Always 0
+ * main - print first 50 Fibonacci numbers
+ * Return: 0
  */
+
 int main(void)
 {
-    int counter;
-    int countto = 50;
-    long a = 1;
-    long b = 2;
+	int counter;
+	int countto = 50;
+	long a = 1;
+	long b = 2;
 
-    for (counter = 1; counter < (countto / 2); counter++)
-    {
-        printf("%li", a);
-        printf(counter != countto / 2 ? ", " : "");
-        printf("%li", b);
-        printf(counter != countto / 2 ? ", " : "");
+	for (counter = 1; counter <= (countto / 2); counter++)
+	{
+		printf("%li %li ", a, b);
+		a += b;
+		b += a;
+	}
+	if (countto % 2 == 1)
+		printf("%li", a);
 
-        a += b;
-        b += a;
-    }
+	printf("\n");
 
-    if (countto % 2 == 1)
-        printf("%li", a);
-
-    printf("\n");
-
-    return (0);
+	return (0);
 }

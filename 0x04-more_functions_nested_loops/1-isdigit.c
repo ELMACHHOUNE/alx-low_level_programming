4423a7e55e499eb0b7dfe5333654a9c59dbedbef
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-03-23 09:07:45 +0000

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,10 +6,10 @@
  * Return: 1 if true, 0 if false
  */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if (c >= '0' && c <= '9')
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }

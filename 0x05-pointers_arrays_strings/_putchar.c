b5318ff423a220cc2a8fe2279c7d1a084084d4c3
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-03-27 13:46:15 +0000

    Update _putchar.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -11,26 +11,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int n = 0;
-	int i;
+	int len, i;
 
-	while (src[n] != '\0')
+	len = 0;
+
+	while (src[len] != '\0')
 	{
-		n++;
+		len++;
 	}
 
-	i = 0;
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+	dest[i] = '\0';
 
-	i = i;
-	while (i <= n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
 	return (dest);
 }

cc9ec7374f8abb471b7d577307dcd25eff2fcdb8
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-04-03 09:29:47 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,27 +12,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0' && s[i] != c) /* find match */
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
 		i++;
-
-	if (s[i] == c) /* if match, assign to address */
-		return (&s[i]);
-	else
-		return (NULL);
-}
-
-/* pointer arithmetic version
-
-char *_strchr(char *s, char c)
-{
-        int i = 0;
-
-	while (*(s + i) && *(s + i) != c)
-	    i++;
-
+	}
 	if (*(s + i) == c)
-	    return (s + i);
-	else
-	    return (NULL);
+		return (s + i);
+	return (NULL);
 }
-*/
